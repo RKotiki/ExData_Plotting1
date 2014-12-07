88ca5ebe05c9c30bf229cd965643b31fa7be45ef
Rohan <RKotiki@users.noreply.github.com>
2014-12-07 22:03:30 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,4 @@
-# Downloaded text file has to be saved in the working directoty
+# Downloaded text file has to be saved in the working directory
 # Read full dataset
 x1<-read.table("./household_power_consumption.txt",sep=";",header=TRUE,na.strings="?",stringsAsFactors=F)
 
@@ -6,7 +6,8 @@ x1<-read.table("./household_power_consumption.txt",sep=";",header=TRUE,na.string
 x2<-x1[x1$Date %in% c("1/2/2007","2/2/2007"),]
 
 # Plotting the data
-hist(x2$Global_active_power, main="Global Active Power", xlab="Global Active Power(kilowatts)", ylab="Frequency", col="Red") 
+hist(x2$Global_active_power, main="Global Active Power", xlab="Global Active Power(kilowatts)", ylab="Frequency",
+col="Red") 
 
 # Graph
 dev.copy(png, file="plot1.png", height=480, width=480) 
